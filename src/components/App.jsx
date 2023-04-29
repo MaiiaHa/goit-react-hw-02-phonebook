f59d8ad7e9d0d4bf6a22e0278fb86be32ee02da2
MaiiaHa <maiia.robeiko@ukr.net>
2023-04-30 00:16:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import Section from './section/Section';
+import Statistics from './statistics/Statistics';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      <Section title={'1 - Feedback widget'}>
+        {
+          <Statistics
+            good={5}
+            neutral={5}
+            bad={5}
+            total={5}
+            positivePercentage={5}
+          />
+        }
+        {/* {children for each Statistics FeedbackOptions} */}
+      </Section>
+
+      <Section title={'1 - Feedback options'}>
+        {/* <FeedbackOptions options={} onLeaveFeedback={}/> */}
+      </Section>
     </div>
   );
 };
