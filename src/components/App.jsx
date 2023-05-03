1453d7cc507eb9552b5eca9a98ee31a03c097084
MaiiaHa <maiia.robeiko@ukr.net>
2023-05-04 00:29:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,61 @@
-import Section from './section/Section';
-import Statistics from './statistics/Statistics';
+import React, { Component } from 'react';
+import Container from './Container/Container';
+import ContactForm from './ContactForm/ContactForm';
+// import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
 
-export const App = () => {
-  return (
-    <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
-    >
-      <Section title={'1 - Feedback widget'}>
-        {
-          <Statistics
-            good={5}
-            neutral={5}
-            bad={5}
-            total={5}
-            positivePercentage={5}
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  deleteContact = userId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(({ id }) => userId !== id),
+    }));
+  };
+
+  newUser = data => {
+    console.log(data);
+    //з затримкою в асинхронному коді теж працює:
+    // setTimeout(() => {
+    //   console.log(data);
+    // }, 1000);
+  };
+
+  addContact = () => {
+    // return this.state.contacts;
+    const newUser = this.state;
+    console.log(newUser);
+
+    this.setState(prevState => ({
+      contacts: [this.state.contacts, ...prevState.contacts],
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Container title={'Phonebook'}>
+          <ContactForm onSubmit={this.newUser} />
+        </Container>
+
+        <Container title={'Contacts'}>
+          {/* <Filter /> */}
+          <ContactList
+            contacts={this.state.contacts}
+            deleteContact={this.deleteContact}
           />
-        }
-        {/* {children for each Statistics FeedbackOptions} */}
-      </Section>
+        </Container>
+      </div>
+    );
+  }
+}
 
-      <Section title={'1 - Feedback options'}>
-        {/* <FeedbackOptions options={} onLeaveFeedback={}/> */}
-      </Section>
-    </div>
-  );
-};
+export default App;
