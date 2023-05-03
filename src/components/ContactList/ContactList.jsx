edce94e05ee96f9192b89c88ea433b591b5a5759
MaiiaHa <maiia.robeiko@ukr.net>
2023-05-04 00:29:26 +0200

    Create ContactList.jsx

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,30 @@
+// import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const ContactList = ({ contacts, deleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          {name}: {number}
+          <button type="button" onClick={() => deleteContact(id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  deleteContact: PropTypes.func.isRequired,
+};
