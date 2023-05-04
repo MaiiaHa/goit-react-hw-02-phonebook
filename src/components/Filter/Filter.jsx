2349644c5ac9947e9d3133620e9afa3efdaaf41f
MaiiaHa <maiia.robeiko@ukr.net>
2023-05-04 22:22:37 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,2 +1,21 @@
 // import React, { Component } from 'react';
 // import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+
+const Filter = ({ value, onChange }) => {
+  return (
+    <label>
+      Find contacts by name
+      <input type="text" name="filter" value={value} onChange={onChange} />
+    </label>
+  );
+};
+
+Filter.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+export default Filter;
+
+// onChange={onChange}
